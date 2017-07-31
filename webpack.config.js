d24eac816887ec3e0eb0a09af58f3c1a1057a56e
Philip Scott <pscott@zeptohost.com>
2017-07-31 13:21:47 -0400

    Created simple functional boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    app: './src/index.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'dist')
+  }
+};
